eb3b6bf6f85ff8f508aef504d1c346524b28ed80
roum02 <roum02@ewhain.net>
2025-04-19 05:38:53 +0900

    Add react hook form

--- src/app/register/client.tsx
+++ src/app/register/client.tsx
@@ -1,15 +1,29 @@
 "use client";
 
-import { useState } from "react";
+import { useForm, SubmitHandler, FormProvider } from "react-hook-form";
 import { useRouter } from "next/navigation";
 import { Textarea } from "@blog-client-components";
 import { useCreatePost } from "@/lib/query/post/hooks";
 import { getCategories, CATEGORY_QUERY_KEY } from "@blog-client-query";
 import { useQuery } from "@tanstack/react-query";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
 interface PostRegisterClientProps {
   //categories: { id: number; name: string }[];
 }
+
+interface RegisterInputType {
+  title: string;
+  categoryId: number;
+  content: string;
+}
+
+const schema = yup.object({
+  title: yup.string().required(),
+  categoryId: yup.number().required(),
+  content: yup.string().required(),
+});
 
 export default function PostRegisterClient({}: PostRegisterClientProps) {
   const { data: categories = [] } = useQuery({
@@ -17,26 +31,30 @@ export default function PostRegisterClient({}: PostRegisterClientProps) {
     queryFn: getCategories,
   });
 
-  const [title, setTitle] = useState("");
-  const [categoryId, setCategoryId] = useState("");
-  const [content, setContent] = useState("");
+  const methods = useForm<RegisterInputType>({
+    resolver: yupResolver(schema),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = methods;
 
   const { mutateAsync } = useCreatePost();
   const router = useRouter();
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-
-    if (!title || !categoryId || !content) {
+  const onSubmit: SubmitHandler<RegisterInputType> = async (data) => {
+    if (!data.title || !data.categoryId || !data.content) {
       alert("모든 항목을 입력해주세요.");
       return;
     }
 
     try {
       await mutateAsync({
-        title,
-        content,
-        categoryId: Number(categoryId),
+        title: data.title,
+        content: data.content,
+        categoryId: Number(data.categoryId),
         authorId: 1,
         isPublished: true,
       });
@@ -49,65 +67,66 @@ export default function PostRegisterClient({}: PostRegisterClientProps) {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="space-y-4 max-w-xl p-6">
-      <div>
-        <label
-          htmlFor="title"
-          className="block text-sm font-medium text-gray-700"
-        >
-          제목
-        </label>
-        <input
-          id="title"
-          name="title"
-          type="text"
-          required
-          onChange={(e) => setTitle(e.target.value)}
-          className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500"
-        />
-      </div>
+    <FormProvider {...methods}>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="space-y-4 max-w-xl p-6"
+      >
+        <div>
+          <label
+            htmlFor="title"
+            className="block text-sm font-medium text-gray-700"
+          >
+            제목
+          </label>
+          <input
+            id="title"
+            type="text"
+            {...register("title")}
+            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500"
+          />
+        </div>
 
-      <div>
-        <label
-          htmlFor="category"
-          className="block text-sm font-medium text-gray-700"
-        >
-          카테고리
-        </label>
-        <select
-          id="category"
-          name="category"
-          value={categoryId}
-          onChange={(e) => setCategoryId(e.target.value)}
-          className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500"
-        >
-          <option value="">카테고리를 선택하세요</option>
-          {categories.map((cat) => (
-            <option key={cat.id} value={cat.id}>
-              {cat.name}
-            </option>
-          ))}
-        </select>
-      </div>
+        <div>
+          <label
+            htmlFor="category"
+            className="block text-sm font-medium text-gray-700"
+          >
+            카테고리
+          </label>
+          <select
+            id="category"
+            {...register("categoryId")}
+            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500"
+          >
+            <option value="">카테고리를 선택하세요</option>
+            {categories.map((cat) => (
+              <option key={cat.id} value={cat.id}>
+                {cat.name}
+              </option>
+            ))}
+          </select>
+        </div>
 
-      <div>
-        <label
-          htmlFor="content"
-          className="block text-sm font-medium text-gray-700"
-        >
-          내용
-        </label>
-        <Textarea value={content} onChange={setContent} />
-      </div>
+        <div>
+          <label
+            htmlFor="content"
+            className="block text-sm font-medium text-gray-700"
+          >
+            내용
+          </label>
+          <Textarea />
+        </div>
 
-      <div className="text-right">
-        <button
-          type="submit"
-          className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition"
-        >
-          등록
-        </button>
-      </div>
-    </form>
+        <div className="text-right">
+          <button
+            type="submit"
+            className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition"
+          >
+            등록
+          </button>
+        </div>
+      </form>
+    </FormProvider>
   );
 }
--- src/lib/components/Client/Textarea/index.tsx
+++ src/lib/components/Client/Textarea/index.tsx
@@ -1,11 +1,11 @@
 "use client";
 
+import { Controller, useFormContext } from "react-hook-form";
 import "froala-editor/js/plugins.pkgd.min.js";
 import "froala-editor/css/froala_editor.pkgd.min.css";
 import "froala-editor/css/froala_style.min.css";
 
 import dynamic from "next/dynamic";
-import { useState } from "react";
 
 const FroalaEditor = dynamic(() => import("react-froala-wysiwyg"), {
   ssr: false,
@@ -16,27 +16,33 @@ interface TextareaProps {
   onChange: (val: string) => void;
 }
 
-export default function Textarea({ value, onChange }: TextareaProps) {
-  const handleModelChange = (newContent: string) => {
-    onChange(newContent); // 사용자가 입력한 HTML 저장
-  };
+export default function Textarea() {
+  const { register, control } = useFormContext();
 
   return (
-    <>
-      <FroalaEditor
-        model={value}
-        onModelChange={handleModelChange}
-        config={{
-          placeholderText: "Write something amazing...",
-        }}
-      />
-      {/* <div className="mt-6">
+    <Controller
+      name="content"
+      control={control}
+      render={({ field }) => (
+        <FroalaEditor
+          {...register("content")}
+          model={field.value}
+          onModelChange={field.onChange}
+          config={{
+            placeholderText: "Write something amazing...",
+          }}
+        />
+      )}
+    />
+  );
+}
+
+{
+  /* <div className="mt-6">
         <h2 className="font-bold mb-2">Preview:</h2>
         <div
           className="border p-4"
           dangerouslySetInnerHTML={{ __html: content }}
         />
-      </div> */}
-    </>
-  );
+      </div> */
 }
