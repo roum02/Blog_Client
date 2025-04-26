506d9065aa2f1441d92be2b71faf2510779ef664
roum02 <roum02@ewhain.net>
2025-04-26 11:29:08 +0900

    Add loout button

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,7 @@
+"use client";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
 const REDIRECT_URI = "http://localhost:3000/login/kakao-callback";
 
 const kakaoAuthUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${
@@ -5,11 +9,34 @@ const kakaoAuthUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${
 }&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&response_type=code`;
 
 export default function LoginPage() {
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    try {
+      await axios.post("/auth/logout", {}, { withCredentials: true });
+      alert("로그아웃 완료");
+      router.push("/");
+    } catch (error: any) {
+      alert(
+        `로그아웃 실패: ${error?.response?.data?.message || error.message}`
+      );
+    }
+  };
+
   return (
-    <a href={kakaoAuthUrl}>
-      <button className="bg-yellow-400 text-black px-4 py-2 rounded">
-        카카오 로그인
+    <>
+      <a href={kakaoAuthUrl}>
+        <button className="bg-yellow-400 text-black px-4 py-2 rounded">
+          카카오 로그인
+        </button>
+      </a>
+
+      <button
+        className="bg-gray-800 text-white px-4 py-2 rounded"
+        onClick={handleLogout}
+      >
+        카카오 로그아웃
       </button>
-    </a>
+    </>
   );
 }
